6719902dbdccaff6f2c2d7e9703c94bc965b7a52
emgrise <160220704+emgrise@users.noreply.github.com>
2024-02-20 10:11:31 +0200

    completed

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def Test (str text):
+def rev_str_only_alf(text):
     text_list = text.split(' ')
 
     new_list = []
@@ -6,21 +6,18 @@ def Test (str text):
         new_word = ''
         letters = [char for char in word if char.isalpha()]
         digits = [char for char in word if char.isdigit()]
+        symbols = [char for char in word if not char.isalpha() and not char.isdigit()]
         for char in word:
             if char.isalpha():
                 new_word += letters.pop()
-            else:
+            elif char.isdigit():
                 new_word += digits.pop(0)
+            else:
+                new_word += symbols.pop(0)
         new_list.append(new_word)
 
     reversed_text = ' '.join(new_list)
     return reversed_text
-
-
-
-
-
-
 
 
 
