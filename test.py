17c891db69a8dffa1caede2d916bd0329717cb74
emgrise <160220704+emgrise@users.noreply.github.com>
2024-02-20 07:31:07 +0200

    Update test.py
    Add func

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
-text = input("New string: ")
-text_list = text.split(' ')
+def Test (str text):
+    text_list = text.split(' ')
 
-new_list = []
-for word in text_list:
-    new_word = ''
-    letters = [char for char in word if char.isalpha()]
-    digits = [char for char in word if char.isdigit()]
-    for char in word:
-        if char.isalpha():
-            new_word += letters.pop()
-        else:
-            new_word += digits.pop(0)
-    new_list.append(new_word)
+    new_list = []
+    for word in text_list:
+        new_word = ''
+        letters = [char for char in word if char.isalpha()]
+        digits = [char for char in word if char.isdigit()]
+        for char in word:
+            if char.isalpha():
+                new_word += letters.pop()
+            else:
+                new_word += digits.pop(0)
+        new_list.append(new_word)
 
-reversed_text = ' '.join(new_list)
-print(reversed_text)
+    reversed_text = ' '.join(new_list)
+    return reversed_text
 
 
 
